419d70491b8cb51c4c6694e775d3dcf6bfd859d2
ashutosh ojha <ashutoshojha@ashutoshs-MacBook-Pro.local>
2018-11-07 01:06:58 +0530

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("hello ashutosh " +args[0]);
+        System.out.print("hello i am just getting started" );
     }
 }
