e227ed91755289c0222ba5e5981f034f25e0865a
ashutosh ojha <ashutoshojha@ashutoshs-MacBook-Pro.local>
2018-11-14 01:58:51 +0530

    duplicate no question done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,34 @@ public class Main {
         System.out.print("hello ashutosh " + args[0]);
         System.out.println("hello i am just getting started");
 
-        int a[]={1,2,9,3,6,7};
-        BitsetSeries.bitset(a,9);
+        int a[] = {1, 2, 9, 3, 6, 7};
+     //   BitsetSeries.bitset(a, 9);
+
+
+        int b[] = {1, 2, 2, 3, 6,6, 7};
+
+        Main.duplicateNumbers(b);
+
+
+    }
+
+     private static void duplicateNumbers(int[] b) {
+
+        int initalNo;
+        if(b.length>0)
+        {
+           initalNo=b[0];
+
+            for(int i=1;i<b.length;i++){
+                if(initalNo==b[i]){
+                    System.out.println("Duplicate number"+initalNo);
+
+                }else {
+                    initalNo=b[i];
+                }
+            }
+        }
+
 
     }
 
